f3862e60be1c2448502447c7b883e64a595b87b9
no <no>
2021-11-17 21:08:53 +0000

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use dialoguer::console;
+use rand::{prelude::SliceRandom, thread_rng};
+
+#[derive(Debug)]
+enum Choice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+// struct Round {
+//     machine_choice: Option<Choice>,
+//     human_choice: Option<Choice>,
+// }
+
 fn main() {
-    println!("Hello, world!");
+    let labels = &["R", "P", "S"];
+
+    let sel: usize = dialoguer::Select::new()
+        .with_prompt("Your choice")
+        .items(&labels[..])
+        .interact()
+        .unwrap();
+
+    let selection = labels[sel];
+
+    let choices = [Choice::Rock, Choice::Paper, Choice::Scissors];
+    let mut rng = thread_rng();
+    let choice = choices.choose(&mut rng).unwrap();
+    println!("{}", selection);
+    println!("{:#?}", choice);
 }
