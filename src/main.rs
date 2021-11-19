8d6c4f2edb39a799e69c1b8a7e7e0fffdc9f414f
no <no>
2021-11-19 15:11:39 +0000

    fix

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ fn main() {
     let human_choice = get_choice_from_prompt();
 
     println!("My choice  : {:#?}", computer_choice);
-    let game_result = get_game_result(computer_choice, human_choice);
-    match game_result.0 {
+    let (winner, message) = get_game_result(computer_choice, human_choice);
+    match winner {
         GameResult::Computer => println!("I am the winner!"),
         GameResult::Human => println!("You are the winner!"),
         GameResult::Tie => println!("It is a tie"),
     }
-    if let Some(message) = game_result.1 {
+    if let Some(message) = message {
         println!("'{}'", message);
     }
 }
